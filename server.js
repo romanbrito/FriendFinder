f23621d213dfd257ba6e6c616b115c99b0039b67
Roman Brito <romanbrito1@gmail.com>
2017-04-17 08:28:11 -0500

    initial directories

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 3000 );
+
+app.use(require('./app/routing/htmlRoutes'));
+
+var server = app.listen(app.get('port'), function() {
+    console.log('Listening on port ' + app.get('port'));
+});
