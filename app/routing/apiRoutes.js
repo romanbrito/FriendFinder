4cebaf76501118227c1995281266745ec26537bd
Roman Brito <romanbrito1@gmail.com>
2017-04-18 20:34:31 -0500

    home, survey and routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var bodyParser = require("body-parser");
+var fs = require('fs');
+var friendsData = require('../data/friends.json');// read json file
 
 // Sets up the Express app to handle data parsing
 router.use(bodyParser.json());
@@ -9,9 +11,18 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.text());
 router.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+router.get('/api/friends', function (req, res) {
+    res.json(friendsData);
+});
+
 router.post('/api/new', function(req, res) {
     var newuser = req.body;
+    friendsData.push(newuser);
+    fs.writeFile('app/data/friends.json', JSON.stringify(friendsData), 'utf8', function (err) { //save new user in file
+       console.log(err);
+    });
     console.log(newuser);
+    res.json(newuser);
 });
 
 module.exports = router;
