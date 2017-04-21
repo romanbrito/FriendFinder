83b5157fc0401b617b23361bc577fe8709f2a38b
Roman Brito <romanbrito1@gmail.com>
2017-04-20 22:05:32 -0500

    added validation and friend logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,14 +15,37 @@ router.get('/api/friends', function (req, res) {
     res.json(friendsData);
 });
 
-router.post('/api/new', function(req, res) {
+router.post('/api/friends', function(req, res) {
+    var friends = friendsData;
+    function sumArraysABS ( array1, array2) {
+        var arrDiff = array1.map(function (num, idx) {
+            return Math.abs(num - array2[idx]);
+        });
+        var sum = arrDiff.reduce(function (a, b) {
+            return a + b;
+        }, 0);
+        return sum;
+    }
     var newuser = req.body;
+
+    var bestMatcharr = [];
+
+    for (var i = 0; i < friends.length; i++) {
+        var sum = sumArraysABS(newuser.scores, friends[i].scores);
+        friends[i].diff = sum;
+        bestMatcharr.push(friends[i]);
+    }
+
+    bestMatcharr.sort(function (a, b) {
+        return a.diff - b.diff;
+    });
+
+    res.json(bestMatcharr[0]);
+
     friendsData.push(newuser);
     fs.writeFile('app/data/friends.json', JSON.stringify(friendsData), 'utf8', function (err) { //save new user in file
        console.log(err);
     });
-    console.log(newuser);
-    res.json(newuser);
 });
 
 module.exports = router;
